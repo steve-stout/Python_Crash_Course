fffaa15e7315bed4b6231ccd09af79186337b7c4
steve-stout <124915281+steve-stout@users.noreply.github.com>
2025-09-16 12:00:49 -0600

    Create stripping_names_2-7.py

---
+++ stripping_names_2-7.py
@@ -0,0 +1,13 @@
+name = "\tSteve Stout\n"
+
+print("Unmodified")
+print(name)
+
+print("\nUsing lstrip():")
+print(name.lstrip())
+
+print("\nUsing rstrip():")
+print(name.rstrip())
+
+print("\nUsing strip():")
+print(name.strip())
