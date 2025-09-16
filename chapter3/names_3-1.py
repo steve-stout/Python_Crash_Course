cddc04eee167593f8339b1ea360fe6a17ecb4eec
steve-stout <124915281+steve-stout@users.noreply.github.com>
2025-09-16 12:50:31 -0600

    Create names_3-1.py

---
+++ chapter3/names_3-1.py
@@ -0,0 +1,14 @@
+names = ['kim','corriey','michael','mike','nolan','david']
+print(names[0].title())
+print(names[1].title())
+print(names[2].title())
+print(names[3].title())
+print(names[4].title())
+print(names[-1].title())
+print(names[-2].title())
+message1 = f"My oldest friend was {names[3].title()}."
+
+message2 = f"{names[2].title()} is my friend from school."
+message3 = f"{names[-2].title()} is my friend from school."
+message4 = f"{names[-1].title()} is a friend from jr high."
+print(message1,message2,message3,message4)
